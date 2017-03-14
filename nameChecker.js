a03c62f5ab98bd43ec463b1e88b5520bd6e2e059
blues <blues@blues.com>
2017-03-14 16:14:32 +0800

    完成基本检查功能

---
+++ nameChecker.js
@@ -0,0 +1,60 @@
+bioPattern = {'name' : ["string"],
+								'role' : ["string"],
+								'biopic' : ["string"],
+								'contacts' : ["object", {
+									"mobile" : ["string"],
+									"email" : ["string"],
+									"github" : ["string"],
+									"location" : ["string"]
+								}],
+								'welcomeMessage' : ["string"],
+								'skills' : ["array"],
+								'biopic' : ["string"],
+								'display' : ["function"]}
+
+eductionPattern = {
+	'schools': ["arrayObject", {
+		"name": ["string"],
+		"location": ["string"],
+		"degree": ["string"],
+		"majors": ["array"],
+		"dates": ["string"],
+		"url": ["string"]
+	}],
+	'onlineCourses': ["arrayObject", {
+		"title": ["string"],
+		"school": ["string"],
+		"dates": ["string"],
+		"url": ["string"]
+	}],
+	'display': ["function"]
+};
+
+function checkPattern(pattern, objectToCheck) {
+	Object.keys(pattern).forEach(function(key) {
+		if (!objectToCheck.hasOwnProperty(key)) {
+			console.log('object do not have %s key', key);
+		} else {
+			if (pattern[key][0] == 'object') {
+				checkPattern(pattern[key][1], bio[key])
+			} else if (pattern[key][0] == 'arrayObject') {
+				if ($.type(objectToCheck[key]) === 'array'){
+					objectToCheck[key].forEach(function (object){
+						checkPattern(pattern[key][1], object);
+					});			
+				};
+			} else {
+				checkType(objectToCheck[key], pattern[key][0]);
+			}
+		}
+	});
+}
+
+function checkType(value, type) {
+	if ($.type(value) !== type) {
+		console.log("%s should be typeof %s", value, type);
+	}
+}
+
+checkPattern(bioPattern, bio);
+checkPattern(eductionPattern, education);
